d730285a1193c67fad356f30ec0efc22dc3cbb43
soulevil09 <igorgalloseabra@gmail.com>
2022-11-09 20:29:25 -0300

    Parte 1

---
+++ empresa/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Empresa, Tecnologias, Vagas
+
+admin.site.register(Empresa)
+admin.site.register(Tecnologias)
+admin.site.register(Vagas)
+# Register your models here.
